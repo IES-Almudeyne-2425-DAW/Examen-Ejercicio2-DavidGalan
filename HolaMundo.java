c6d6e37af9a425b0f023a55c5382ae2042dc5189
Daviid81 <davidgalvalle@gmail.com>
2024-11-06 08:33:47 +0100

    Conflicto ahora si

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,5 +4,7 @@ public class HolaMundo {
 
         System.out.println
         ("Hola soy la frase para generar conflicto, aqui esta el conflicto");
+
+        System.out.println("Segundo intento");
     }
 }
