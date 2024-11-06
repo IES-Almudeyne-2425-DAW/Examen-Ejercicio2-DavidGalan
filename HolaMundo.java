a0f0a4726311d93afa20f775586aff5c5bbd84e4
Daviid81 <davidgalvalle@gmail.com>
2024-11-06 08:32:10 +0100

    conflicto

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo hacer conflicto");
 
-        System.out.println("Hola soy la frase para generar conflicto");
+        System.out.println
+        ("Hola soy la frase para generar conflicto, aqui esta el conflicto");
     }
 }
